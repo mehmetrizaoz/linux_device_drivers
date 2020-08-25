99d07b218aeebdf7a793d990375a6236eae17683
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-08-25 16:38:33 +0300

    module parameters and parameter change callback

---
+++ moduleParameters/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+####################################################
+# test.sh    : hello_world driver
+# Usage      : sh drive.sh &
+#
+# Todo List  :
+#
+# Date       : 25/08/2020
+# Author     : Mehmet Rıza ÖZ - mehmetrizaoz@gmail.com
+#####################################################
+
+insmod hello_world_module.ko
+echo "------------------------------"
+
+echo 13 > /sys/module/hello_world_module/parameters/cb_valueETX
+
+echo "------------------------------"
+dmesg
+echo "------------------------------"
+modinfo hello_world_module.ko
+echo "------------------------------"
+rmmod hello_world_module.ko
+
