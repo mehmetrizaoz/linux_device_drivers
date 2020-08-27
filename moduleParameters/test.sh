b18410fdb68bd5bb61973a2e28a51edaf2e9b61c
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-08-28 00:11:47 +0300

    test script corrections

--- moduleParameters/test.sh
+++ moduleParameters/test.sh
@@ -8,16 +8,24 @@
 # Date       : 25/08/2020
 # Author     : Mehmet Rıza ÖZ - mehmetrizaoz@gmail.com
 #####################################################
-
+clear
+echo "------------------------------"
+modinfo hello_world_module.ko 
 insmod hello_world_module.ko
 echo "------------------------------"
-
-echo 13 > /sys/module/hello_world_module/parameters/cb_valueETX
-
+lsmod | grep hello_world
 echo "------------------------------"
-dmesg
+echo 8 > /sys/module/hello_world_module/parameters/cb_valueETX
+echo 9 > /sys/module/hello_world_module/parameters/valueETX
+echo 10,11,12,13 > /sys/module/hello_world_module/parameters/arr_valueETX
+echo "mehmet" > /sys/module/hello_world_module/parameters/nameETX
 echo "------------------------------"
-modinfo hello_world_module.ko
+dmesg | tail  -10
+echo "------------------------------"
+cat /sys/module/hello_world_module/parameters/cb_valueETX
+cat /sys/module/hello_world_module/parameters/valueETX
+cat /sys/module/hello_world_module/parameters/arr_valueETX
+cat /sys/module/hello_world_module/parameters/nameETX
 echo "------------------------------"
 rmmod hello_world_module.ko
 
