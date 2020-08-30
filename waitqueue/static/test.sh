d9cc68f893fa2da84f6191babfc0f7005054f4dd
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-08-31 00:24:27 +0300

    waitqueue

---
+++ waitqueue/static/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+####################################################
+# test.sh    : hello_world driver
+# Usage      : sudo ./test.sh &
+#
+# Todo List  :
+#
+# Date       : 25/08/2020
+# Author     : Mehmet Rıza ÖZ - mehmetrizaoz@gmail.com
+#####################################################
+clear
+insmod hello_world_module.ko
+cat /dev/myDevice
+cat /dev/myDevice
+cat /dev/myDevice
+cat /dev/myDevice
+cat /dev/myDevice
+cat /dev/myDevice
+cat /dev/myDevice
+rmmod hello_world_module.ko
+dmesg
+
